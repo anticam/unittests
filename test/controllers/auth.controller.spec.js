964e37952cd0866ccc2e214fff0b4386be54ff39
Antal P <antal.perger@gmail.com>
2023-08-02 08:45:43 +0200

    update

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,14 +1,24 @@
 var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
+
 describe('AuthController', function () {
+
+    beforeEach(function () {
+        console.log('running before each');
+        authController.setRoles(['user']);
+    })
+
     describe('isAuthorized', function () {
+
         it('Should return false if not authorized', function () {
-            assert.equal(false, authController.isAuthorized(['user'], 'admin'))
+            //authController.setRoles(['user']);
+            assert.equal(false, authController.isAuthorized('admin'))
         })
 
         it('Should return true if authorized', function () {
-            assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'))
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorized('admin'))
         })
 
 
@@ -16,7 +26,9 @@ describe('AuthController', function () {
 
     describe('isAuthorizedAsync', function () {
         it('Should return false if not authorized', function (done) {
-            authController.isAuthorizedAsync(['user'], 'admin',
+            //this.timeout(2500);
+            //authController.setRoles(['user']);
+            authController.isAuthorizedAsync('admin',
                 function (isAuth) {
                     assert.equal(false, isAuth);
                     done();
